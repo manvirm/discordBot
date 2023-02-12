3efd18823797446c2e61eaa9617a9f3c55951414
Manvir Mann <manvirm29@gmail.com>
2023-02-11 23:19:34 -0800

    Listen for messages and respond

--- main.py
+++ main.py
@@ -3,16 +3,31 @@ import os
 from dotenv import load_dotenv
 from neuralintents import GenericAssistant
 
+# Train model on intents file
+chatbot = GenericAssistant('intents.json')
+chatbot.train_model()
+chatbot.save_model()
+
+print("AI Bot running...")
+
+# Create discord client
 client = discord.Client()
+
+# Load bot token
 load_dotenv()
 TOKEN = os.getenv('TOKEN')
 
+# Listen for incoming messages
 @client.envt
 async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content.startswith("#aibot"):
-        pass
+    # If message starts with '$aibot' then respond
+    if message.content.startswith("$aibot"):
+        # Cut off first 7 characters to ignore '$aibot ' 
+        response  = chatbot.request(message.content[7:])
+        await message.channel.send(response)
 
+# Call that runs client
 client.run(TOKEN)
